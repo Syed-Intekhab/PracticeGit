f186669e7aab3ff82651e392bf99cf868df3b333
Syed Intekhab <intekhab.dev@gmail.com>
2025-10-20 10:31:21 +0530

    Initial Commit

--- StudentPortal/Controllers/HomeController.cs
+++ StudentPortal/Controllers/HomeController.cs
@@ -18,6 +18,16 @@ namespace StudentPortal.Controllers
             return View();
         }
 
+        public IActionResult Details()
+        {
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
         public IActionResult Privacy()
         {
             return View();

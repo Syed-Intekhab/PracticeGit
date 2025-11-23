3b11a21ff75de8a85256bc0b35a198a6d4a0b84d
Syed Intekhab <intekhab.dev@gmail.com>
2025-11-23 11:55:16 +0530

    Practice web api with db EF core (DB First Approach).

---
+++ CRUDPracticeAPI/Models/Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CRUDPracticeAPI.Models;
+
+public partial class Student
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public int Age { get; set; }
+
+    public int Standard { get; set; }
+
+    public string FatherName { get; set; } = null!;
+}

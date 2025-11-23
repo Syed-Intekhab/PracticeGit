52f1e27eb7a526e14d89deef52001776e1aba214
Syed Intekhab <intekhab.dev@gmail.com>
2025-11-23 12:07:08 +0530

    Added project to consume api with ef

---
+++ ConsumeCRUDPracticeAPI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ConsumeCRUDPracticeAPI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

efe6c8aaf83bd9ba32656fff566ef819d33207b1
Syed Intekhab <intekhab.dev@gmail.com>
2025-12-15 12:55:36 +0530

    Milestone 6 Classes and Objects - Completed Milestone 7 Constructors and Encapsulation - Completed

---
+++ ExpenseTracker/Models/Expense.cs
@@ -0,0 +1,10 @@
+﻿namespace ExpenseTracker.Models
+{
+    internal class Expense(int amount, string? name, string? category)
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public int Amount { get; set; } = amount;
+        public string? Name { get; set; } = name;
+        public string? Category { get; set; } = category;
+    }
+}
--- ExpenseTracker/Program.cs
+++ ExpenseTracker/Program.cs
@@ -1,11 +1,55 @@
-﻿namespace ExpenseTracker
+﻿using ExpenseTracker.Models;
+
+namespace ExpenseTracker
 {
     class Program
     {
         static void Main(string[] args)
         {
+            //Milestone 1/2/3/4/5
+            #region
             // int budget;
-            int[] expenses = new int[10];
+            //int[] expenses = new int[10];
+            //int index = 0;
+
+            //// Take user input for name and budget
+            //Console.Write("Enter your name: ");
+            //string? name = Console.ReadLine();
+
+            //// Display welcome message
+            //Console.WriteLine($"Welcome {name}! Your monthly budget is {GetValidAmount("Enter your monthly budget: ")}.\n");
+
+            //// Show menu until user decides to exit
+            //while (true)
+            //{
+            //    DisplayMenu();
+            //    int choice = int.Parse(Console.ReadLine() ?? "0");
+
+            //    switch (choice)
+            //    {
+            //        case 1: 
+            //            expenses[index++] = GetValidAmount("Enter expense amount: ");
+            //            break;
+            //        case 2:
+            //            foreach (var expense in expenses)
+            //            {
+            //                if (expense != 0)
+            //                    Console.WriteLine($"Expense: {expense}");
+            //            }
+            //            break;
+            //        case 3:
+            //            Console.WriteLine("Exiting the application. Goodbye!");
+            //            return;
+            //        default:
+            //            Console.WriteLine("Invalid option. Please try again.");
+            //            break;
+            //    }
+            //}
+            #endregion
+
+            //Milestone 6
+            #region
+            Expense[] expenses = new Expense[10];
             int index = 0;
 
             // Take user input for name and budget
@@ -23,14 +67,29 @@
 
                 switch (choice)
                 {
-                    case 1: 
-                        expenses[index++] = GetValidAmount("Enter expense amount: ");
+                    case 1:
+                        if(index >= expenses.Length)
+                        {
+                            Console.WriteLine("Expense limit reached. Cannot add more expenses.");
+                            break;
+                        }
+                        Console.Write("Enter expense name: ");
+                        string? expenseName = Console.ReadLine();
+                        int expenseAmount = GetValidAmount("Enter expense amount: ");
+                        Console.Write("Enter expense category: ");
+                        string? expenseCategory = Console.ReadLine();
+
+                        expenses[index++] = new Expense(expenseAmount, expenseName, expenseCategory);
                         break;
                     case 2:
                         foreach (var expense in expenses)
                         {
-                            if (expense != 0)
-                                Console.WriteLine($"Expense: {expense}");
+                            if (expense != null && expense.Amount != 0)
+                            {
+                                Console.WriteLine($"Name: {expense.Name}");
+                                Console.WriteLine($"Expense: {expense.Amount}");
+                                Console.WriteLine($"Category: {expense.Category}\n");
+                            }
                         }
                         break;
                     case 3:
@@ -41,6 +100,7 @@
                         break;
                 }
             }
+            #endregion
         }
 
         // Method to get and validate budget input
